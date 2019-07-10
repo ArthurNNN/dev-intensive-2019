c66b19b607f6ffc219ff1008fc54ed35f4bad064
Arthur <nikitinarthur@gmail.com>
2019-07-10 21:54:25 +0200

    hometask_2 complete

---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -0,0 +1,68 @@
+package ru.skillbranch.devintensive.extensions
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+
+const val SECOND = 1000L
+const val MINUTE= 60 * SECOND
+const val HOUR = 60 * MINUTE
+const val DAY = 24* HOUR
+
+
+
+fun Date.format(pattern:String="HH:mm:ss dd.MM.yy"):String {
+    val dateFormat = SimpleDateFormat(pattern, Locale("ru"))
+    return dateFormat.format(this)
+}
+
+fun Date.add(value:Int, units: TimeUnits = TimeUnits.SECOND) : Date {
+    var time = this.time
+
+    time +=when(units) {
+        TimeUnits.SECOND -> value*SECOND
+        TimeUnits.MINUTE -> value*MINUTE
+        TimeUnits.HOUR -> value*HOUR
+        TimeUnits.DAY -> value*DAY
+        else -> throw IllegalStateException("invalid unit")
+    }
+    this.time =  time
+    return this
+
+}
+
+fun Date.humanizeDiff(): String {
+    val minutePlural = listOf("минут", "минуту", "минуты", "минуты", "минуты", "минут", "минут", "минут", "минут", "минут", "минут")
+    val time = this.time
+    val days = time / DAY
+    val hours = time / HOUR
+    val minutes = time / MINUTE
+    val seconds = time / SECOND
+
+    val currentTime = Date().time
+    val currentDays = currentTime / DAY
+    val currentHours = currentTime / HOUR
+    val currentMinutes = currentTime / MINUTE
+    val currentSeconds = currentTime / SECOND
+
+    return when {
+        currentSeconds - seconds in 0..1 -> "только что"
+        currentSeconds - seconds in 1..45 -> "несколько секунд назад"
+        currentSeconds - seconds in 45..75 -> "минуту назад"
+        currentSeconds - seconds >= 75 && currentMinutes - minutes <= 45 -> "${currentMinutes - minutes} минуту назад"
+        currentMinutes - minutes in 45..75 -> "час назад"
+        currentMinutes - minutes >= 75 && currentHours - hours <= 22 -> "${currentHours - hours} часа назад"
+        currentHours - hours in 22..26 -> "день назад"
+        currentHours - hours >= 26 && currentDays - days <= 360 -> "${currentDays - days} дней назад"
+        currentDays - days > 360 -> "более года назад"
+        else -> "более года назад"
+    }
+}
+
+enum class TimeUnits{
+    SECOND,
+    MINUTE,
+    HOUR,
+    DAY
+}
---
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -0,0 +1,170 @@
+package ru.skillbranch.devintensive.utils
+
+object Utils {
+
+    private val mapToTranslit =
+        mapOf(
+            "а" to "a",
+
+            "б" to "b",
+
+            "в" to "v",
+
+            "г" to "g",
+
+            "д" to "d",
+
+            "е" to "e",
+
+            "ё" to "e",
+
+            "ж" to "zh",
+
+            "з" to "z",
+
+            "и" to "i",
+
+            "й" to "i",
+
+            "к" to "k",
+
+            "л" to "l",
+
+            "м" to "m",
+
+            "н" to "n",
+
+            "о" to "o",
+
+            "п" to "p",
+
+            "р" to "r",
+
+            "с" to "s",
+
+            "т" to "t",
+
+            "у" to "u",
+
+            "ф" to "f",
+
+            "х" to "h",
+
+            "ц" to "c",
+
+            "ч" to "ch",
+
+            "ш" to "sh",
+
+            "щ" to "sh'",
+
+            "ъ" to "",
+
+            "ы" to "i",
+
+            "ь" to "",
+
+            "э" to "e",
+
+            "ю" to "yu",
+
+            "я" to "ya",
+            " " to "_"
+        )
+    private val mapFromTranslit =
+        mapOf(
+            "a" to "а",
+
+            "b" to "б",
+
+            "v" to "в",
+
+            "g" to "г",
+
+            "d" to "д",
+
+            "e" to "е",
+
+            "e" to "ё",
+
+            "zh" to "ж",
+
+            "z" to "з",
+
+            "i" to "и",
+
+            "i" to "й",
+
+            "k" to "к",
+
+            "l" to "л",
+
+            "m" to "м",
+
+            "n" to "н",
+
+            "o" to "о",
+
+            "p" to "п",
+
+            "r" to "р",
+
+            "s" to "с",
+
+            "t" to "т",
+
+            "u" to "у",
+
+            "f" to "ф",
+
+            "h" to "х",
+
+            "c" to "ц",
+
+            "ch" to "ч",
+
+            "sh" to "ш",
+
+            "sh'" to "щ",
+
+            "" to "ъ",
+
+            "i" to "ы",
+
+            "e" to "э",
+
+            "yu" to "ю",
+
+            "ya" to "я"
+        )
+
+    fun parseFullName(fullName: String?): Pair<String?, String?> {
+        val parts: List<String>? = fullName?.trim()?.split(" ")
+        val firstName = parts?.getOrNull(0)
+        val lastName = parts?.getOrNull(1)
+        return (if (firstName?.isBlank() == false) firstName else null) to (if (lastName?.isBlank() == false) lastName else null)
+    }
+
+    fun toInitials(firstName: String?, lastName: String?): String? =
+        if (firstName?.isBlank() != false && lastName?.isBlank() != false) {
+            null
+        } else {
+            "${firstName?.get(0)?.toUpperCase() ?: ""}${lastName?.get(0)?.toUpperCase() ?: ""}"
+        }
+
+    fun transliteration(fullName: String?, divider: String = " "): String? {
+        return fullName?.map {
+            var letter: String? = it.toString()
+            letter = if (it.toString() == " ") {
+                divider
+            } else {
+                if (it.isUpperCase()) {
+                    (mapToTranslit[letter?.toLowerCase()] ?: letter)?.capitalize()
+                } else {
+                    mapToTranslit[letter?.toLowerCase()] ?: letter
+                }
+            }
+            letter
+        }?.joinToString(separator = "")
+    }
+}
